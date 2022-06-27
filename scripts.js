fe07a4f957d0cfd2f3f6b0ff992cce6cdcc22a13
Mr-Macko <max.allesch@gmx.de>
2022-06-27 17:46:49 +0200

    added structure for api data

--- scripts.js
+++ scripts.js
@@ -7,5 +7,19 @@ async function fetchProducts() {
 };
 
 fetchProducts();
+    // structures the fetched data
+    let headline = document.getElementById('headline');
+    let image = document.getElementById('image');
+
+    let product1 = product.ricS;
+    let product2 = product.ric;
+    let product3 = product.ricR;
+    let product4 = product.cic;
+
+    let button1 = document.getElementById('button1');
+    let button2 = document.getElementById('button2');
+    let button3 = document.getElementById('button3');
+    let button4 = document.getElementById('button4');
+ 
 
 
